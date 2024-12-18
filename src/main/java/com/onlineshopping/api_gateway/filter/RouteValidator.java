ec39335f6b1a1e3296bf3e3d310d4300aae9a0fb
Sharan B <sderanna@altimetrik.com>
2024-12-18 23:14:39 +0530

    updated route

--- src/main/java/com/onlineshopping/api_gateway/filter/RouteValidator.java
+++ src/main/java/com/onlineshopping/api_gateway/filter/RouteValidator.java
@@ -12,7 +12,8 @@ public class RouteValidator {
     public static final List<String> openApiEndpoints = List.of(
             "/auth/register",
             "/auth/token",
-            "/eureka"
+            "/eureka",
+            "/products/all"
     );
 
     public Predicate<ServerHttpRequest> isSecured =
